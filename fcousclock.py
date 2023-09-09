042af81ac2d67264732f0ec2689154c24aeda0c9
watcher018 <106078224+watcher018@users.noreply.github.com>
2023-09-09 15:09:44 +0800

    Update fcousclock.py

--- fcousclock.py
+++ fcousclock.py
@@ -1,33 +1,18 @@
 import time
 
-def pomodoro_timer():
-    while True:
-        # 专注时间为25分钟
-        work_time = 25 * 60 
-        
-        print("开始专注!")
-        
-        # 倒计时直到专注时间结束
-        for remaining in range(work_time, 0, -1):
-            mins = remaining // 60
-            secs = remaining % 60
-            timer_display = "{:02d}:{:02d}".format(mins, secs)
-            print(timer_display, end='\r')
-            time.sleep(1)
-        
-        print("专注时间结束!")
-        
-        # 短休息5分钟
-        rest_time = 5 * 60
-        print("开始休息5分钟.")
-        for remaining in range(rest_time, 0, -1):
-            mins = remaining // 60
-            secs = remaining % 60
-            timer_display = "{:02d}:{:02d}".format(mins, secs)
-            print(timer_display, end='\r')
-            time.sleep(1)
+def focus_clock(minutes):
+    seconds = minutes * 60
+    end_time = time.time() + seconds
 
-        print("休息时间结束!")
+    while time.time() < end_time:
+        remaining_time = end_time - time.time()
+        minutes = int(remaining_time // 60)
+        seconds = int(remaining_time % 60)
 
-if __name__ == '__main__':
-    pomodoro_timer()
+        print(f"Remaining Time: {minutes:02d}:{seconds:02d}", end='\r')
+        time.sleep(1)
+
+    print("Time's up!")
+
+# 设置专注时间为25分钟
+focus_clock(25)
